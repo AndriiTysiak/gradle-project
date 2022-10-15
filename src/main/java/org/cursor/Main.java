87544142587b9f8c4cd1b730b7a91108882e7170
Andrii Tysiak <tysiak45@gmail.com>
2022-10-15 14:15:29 +0300

    First commit

---
+++ src/main/java/org/cursor/Main.java
@@ -0,0 +1,7 @@
+package org.cursor;
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/main/java/org/cursor/Person.java
@@ -0,0 +1,15 @@
+package org.cursor;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class Person {
+    private int id;
+    private String userName;
+    private String password;
+
+}
